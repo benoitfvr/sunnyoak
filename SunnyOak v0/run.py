7ee3e7f09b3608f184aff5e28678824cf9f7c29f
Benoît <61755435+Mowdyy@users.noreply.github.com>
2022-09-02 18:09:48 +0200

    delete fake 0.5

--- SunnyOak v0/run.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-# démarage de l'instance du site en local
-from soapp import app   # import des ressources du site web dans le dossier soapp 
-
-if __name__ == "__main__":      # flask cherche le nom de l'app pour les ressources du site
-	app.run(debug=True, host='0.0.0.0')       # demarre le serveur sur l'ip de la rpi pour l'utiliser en local
